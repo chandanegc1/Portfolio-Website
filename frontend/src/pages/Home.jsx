d9966c9c4760d1b05078c6b4831611b1f060030b
CHANDAN KANAUJIA <113818031+chandanegc1@users.noreply.github.com>
2024-06-08 20:13:13 +0530

    bottom nav bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,26 @@ import Wrapper from '../styles/Home'
 
 const Home = () => {
   return (
-    <Wrapper>Home</Wrapper>
+    <Wrapper>
+      <html>
+  <body>
+    <div class="container">
+        <div className="header">
+        <header className="main-header">
+            <nav>
+                <ul>
+                    <li><a href="#home">CHRONOLOGICAL</a></li>
+                    <li><a href="#services">PROGRAMMATIC</a></li>
+                    <li><a href="#about">SCALE</a></li>
+                    <li><a href="#contact">STATUS</a></li>
+                </ul>
+            </nav>
+        </header>
+        </div>
+    </div>
+</body>
+</html>
+    </Wrapper>
   )
 }
 
--- frontend/src/styles/Home.js
+++ frontend/src/styles/Home.js
@@ -1,6 +1,44 @@
 import styled from "styled-components"
 
 const Wrapper = styled.article`
-   background:red;
+*{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   color: black;
+}
+a{
+   text-decoration: none;
+}
+body , html{
+   margin: 0;
+   padding: 0;
+}
+
+.main-header {
+   width: 100vw;
+   display: flex;
+   justify-content: center;
+    position: fixed;
+    bottom: 10px;
+    color: black;
+    left: 0;
+    text-align: center;
+    z-index: 1000;
+  }
+
+ nav{
+   width: 50vw;
+   border: 2px solid red;
+   border-radius: 6px;
+}
+
+
+ ul{
+   line-height: 30px;
+   display: flex;
+   justify-content: space-around;
+   list-style: none;
+ }
 `;
-export default Wrapper;
+export default Wrapper; 
